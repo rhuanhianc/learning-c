75c6356ea28fd7d27268cb715ada4abbfb790687
Rhuan S. Hianc <rhuanhianc123@gmail.com>
2024-08-15 18:19:26 -0300

    loops

---
+++ hello_world/loops.c
@@ -0,0 +1,12 @@
+// Contar de 1 a 5
+for (int i = 1; i <= 5; i++) {
+    printf("%d ", i);
+}
+// Vai imprimir: 1 2 3 4 5
+
+// Outra forma de contar até 5
+int j = 1;
+while (j <= 5) {
+    printf("%d ", j);
+    j = j + 1;
+}
